f1ee00ec1fd2fc3a3c5b9268317b4d3177c3d3aa
unknown <nicolas.ssouza2005@gmail.com>
2024-12-15 20:36:57 -0300

    atualizacao

--- paginas/listarAlunos.php
+++ paginas/listarAlunos.php
@@ -7,15 +7,28 @@
 </head>
 <body>
     <h1>Lista de Alunos</h1>
+    
+    <table>
     <?php
     require '../conexao.php';
-
     $sql = "SELECT * FROM alunos ORDER BY nome";
     $stmt = $pdo->query($sql);
 
+    $tabela = "";
+    $tem_registro = False;
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        echo "Nome: " . $row['nome'] . " - Turma: " . $row['turma_id'] . "<br>";
+        $tabela = $tabela . "<tr>";
+        $tabela = $tabela . "<td>".$row['nome']."</td><td>".$row['turma_id']."</td>";
+        $tabela = $tabela . "</tr>";
+        $tem_registro = True;
+    }
+    if ($tem_registro) {
+        echo "<tr><th>Nome</th><th>Turma</th></tr>";
+        echo $tabela;
+    } else {
+        echo "<tr><th>Nenhum Registro Encontrado!</th></tr>";
     }
     ?>
+    </table>
 </body>
 </html>
